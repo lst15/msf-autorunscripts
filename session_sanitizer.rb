8b1e53f2ab1e191b95dbec173669e61b75e83d70
leandro <38253462+lst15@users.noreply.github.com>
2025-12-10 20:43:39 -0300

    Refactor session IP extraction and duplicate killing

--- session_sanitizer.rb
+++ session_sanitizer.rb
@@ -2,34 +2,32 @@
 # 1. Detects and terminates duplicate sessions from the same IP.
 # 2. Provides a minimal API for session-level hygiene.
 
-fm = client.framework
-target_ip = client.session_host
+module SessionSanitizer
+  def self.extract_ip(sess)
+    stream =
+      if sess.respond_to?(:sock)
+        sess.sock
+      elsif sess.respond_to?(:rstream)
+        sess.rstream
+      end
+    return nil unless stream
 
-def extract_ip(sess)
-  stream =
-    if sess.respond_to?(:sock)
-      sess.sock
-    elsif sess.respond_to?(:rstream)
-      sess.rstream
+    begin
+      peer = stream.peerinfo
+      peer&.split(':')&.first
+    rescue IOError, Errno::ENOTCONN
+      nil
     end
-  return nil unless stream
+  end
 
-  begin
-    peer = stream.peerinfo
-    peer&.split(':')&.first
-  rescue IOError, Errno::ENOTCONN
-    nil
+  def self.run(fm, target_ip, current_sid)
+    fm.sessions.each do |sid, sess|
+      next if sid == current_sid
+      ip = extract_ip(sess)
+      next unless ip == target_ip
+
+      print_error("Killing duplicate session ID #{sid} for IP #{target_ip}")
+      sess.kill
+    end
   end
 end
-
-def kill_duplicates(fm, target_ip, current_sid)
-  fm.sessions.each do |sid, sess|
-    next if sid == current_sid
-    ip = extract_ip(sess)
-    next unless ip == target_ip
-    print_error("Killing duplicate session ID #{sid} for IP #{target_ip}")
-    sess.kill
-  end
-end
-
-kill_duplicates(fm, target_ip, client.sid)
